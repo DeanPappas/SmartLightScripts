a80c4797c1ebe17e87902a22f0a0bc8352680e3d
DeanPappas <kpappas302@gmail.com>
2026-01-04 12:55:21 -0600

    lightControlGui updated with instructions and made light names easier to modify

--- lightControlGui.py
+++ lightControlGui.py
@@ -6,7 +6,11 @@ from kivy.properties import ObjectProperty
 Config.set('graphics', 'fullscreen', 'auto')
 from phue import Bridge
 
+# Set IP from Philips Hue Bridge
 b = Bridge('')
+# Set light names from Hue App
+light1 = "Desk Light"
+light2 = "Corner Light"
 b.connect()
 
 kv_text = Builder.load_file("lightGUI.kv")
@@ -19,154 +23,153 @@ class MainScreen(ScreenManager):
 class FirstScreen(Screen):
     
     def powerOn(self):
-        b.set_light('Desk Light','on', True)
-        b.set_light('TV Light','on', True)
+        b.set_light(light1,'on', True)
+        b.set_light(light2,'on', True)
     
     def powerOff(self):
-        b.set_light('Desk Light','on', False)
-        b.set_light('TV Light','on', False)
+        b.set_light(light1,'on', False)
+        b.set_light(light2,'on', False)
 
 class SecondScreen(Screen):
 
-    bri = b.get_light('Desk Light','bri')
-    bri = b.get_light('TV Light','bri')
+    bri = b.get_light(light1,'bri')
+    bri = b.get_light(light2,'bri')
     
     def blue(self):
-        b.set_light('Desk Light','xy', (.01,.01))
-        b.set_light('TV Light','xy', (.01,.01))
+        b.set_light(light1,'xy', (.01,.01))
+        b.set_light(light2,'xy', (.01,.01))
 
     def red(self):
-        b.set_light('Desk Light','xy', (1,.5))
-        b.set_light('TV Light','xy', (1,.5))
+        b.set_light(light1,'xy', (1,.5))
+        b.set_light(light2,'xy', (1,.5))
 
     def green(self):
-        b.set_light('Desk Light','xy', (.5,1))
-        b.set_light('TV Light','xy', (.5,1))
+        b.set_light(light1,'xy', (.5,1))
+        b.set_light(light2,'xy', (.5,1))
 
     def yellow(self):
-        b.set_light('Desk Light','xy', (.5,.5))
-        b.set_light('TV Light','xy', (.5,.5))
+        b.set_light(light1,'xy', (.5,.5))
+        b.set_light(light2,'xy', (.5,.5))
 
     def addBri(self):
         if(self.bri > 240):
             print(self.bri)
             self.bri = 254
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri + 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
     def subBri(self):
         if(self.bri < 20):
             print(self.bri)
             self.bri = 1
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri - 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
 class ThirdScreen(Screen):
 
-    bri = b.get_light('Desk Light','bri')
-    bri = b.get_light('TV Light','bri')
+    bri = b.get_light(light1,'bri')
+    bri = b.get_light(light2,'bri')
     
     def teal(self):
-        b.set_light('Desk Light','xy', (.25,.45))
-        b.set_light('TV Light','xy', (.25,.45))
+        b.set_light(light1,'xy', (.25,.45))
+        b.set_light(light2,'xy', (.25,.45))
 
     def orange(self):
-        b.set_light('Desk Light','xy', (0.614, 0.3783))
-        b.set_light('TV Light','xy', (0.614, 0.3783))
+        b.set_light(light1,'xy', (0.614, 0.3783))
+        b.set_light(light2,'xy', (0.614, 0.3783))
 
     def violet(self):
-        b.set_light('Desk Light','xy', (.3,.1))
-        b.set_light('TV Light','xy', (.3,.1))
+        b.set_light(light1,'xy', (.3,.1))
+        b.set_light(light2,'xy', (.3,.1))
 
     def pink(self):
-        b.set_light('Desk Light','xy', (0.3971, 0.225))
-        b.set_light('TV Light','xy', (0.3971, 0.225))
+        b.set_light(light1,'xy', (0.3971, 0.225))
+        b.set_light(light2,'xy', (0.3971, 0.225))
 
     def addBri(self):
         if(self.bri > 240):
             print(self.bri)
             self.bri = 254
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri + 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
     def subBri(self):
         if(self.bri < 20):
             print(self.bri)
             self.bri = 1
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri - 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
 class FourthScreen(Screen):
     
-    bri = b.get_light('Desk Light','bri')
-    bri = b.get_light('TV Light','bri')
+    bri = b.get_light(light1,'bri')
+    bri = b.get_light(light2,'bri')
    
     def blue(self):
-        b.set_light('Desk Light','xy', (.01,.01))
-        b.set_light('TV Light','xy', (.01,.01))
+        b.set_light(light1,'xy', (.01,.01))
+        b.set_light(light2,'xy', (.01,.01))
 
     def red(self):
-        b.set_light('Desk Light','xy', (1,.5))
-        b.set_light('TV Light','xy', (1,.5))
+        b.set_light(light1,'xy', (1,.5))
+        b.set_light(light2,'xy', (1,.5))
 
     def green(self):
-        b.set_light('Desk Light','xy', (.5,1))
-        b.set_light('TV Light','xy', (.5,1))
+        b.set_light(light1,'xy', (.5,1))
+        b.set_light(light2,'xy', (.5,1))
 
     def yellow(self):
-        b.set_light('Desk Light','xy', (.5,.5))
-        b.set_light('TV Light','xy', (.5,.5))
+        b.set_light(light1,'xy', (.5,.5))
+        b.set_light(light2,'xy', (.5,.5))
 
     def addBri(self):
         if(self.bri > 240):
             print(self.bri)
             self.bri = 254
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri + 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
     def subBri(self):
         if(self.bri < 20):
             print(self.bri)
             self.bri = 1
-            b.set_light('Desk Light','bri',self.bri)
-            b.set_light('TV Light','bri',self.bri)
+            b.set_light(light1,'bri',self.bri)
+            b.set_light(light2,'bri',self.bri)
         else:
             print(self.bri)
             self.bri = self.bri - 20
-            b.set_light('Desk Light', 'bri', self.bri)
-            b.set_light('TV Light', 'bri', self.bri)
+            b.set_light(light1, 'bri', self.bri)
+            b.set_light(light2, 'bri', self.bri)
 
 class MyApp(App):
     def build(self):
         return MainScreen()
 
 def main():
-    b = Bridge('')
     MyApp().run()
 
 if __name__ == '__main__':
